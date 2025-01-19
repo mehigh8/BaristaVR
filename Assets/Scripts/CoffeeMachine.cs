d71a4b37fbd1e674d170a8b99d7442b3a3629c83
Popa Bogdan-Gabriel <bogdan.popa.g25@gmail.com>
2025-01-19 18:50:52 +0200

    fixes and particle system

--- Assets/Scripts/CoffeeMachine.cs
+++ Assets/Scripts/CoffeeMachine.cs
@@ -8,17 +8,37 @@ public class CoffeeMachine : MonoBehaviour
     public float basePouringSpeed;
 
     private bool isPressed = false;
+    
+    [SerializeField] private GameObject particleSystem;
+    [SerializeField] private Material coffeeColor;
+    private ParticleSystem currentParticleSystem;
 
+    private void Start()
+    {
+        currentParticleSystem = Instantiate(particleSystem).GetComponent<ParticleSystem>();
+        ParticleSystem.EmissionModule emission = currentParticleSystem.emission;
+        emission.rateOverTime = 0f;
+        currentParticleSystem.GetComponent<Renderer>().material = coffeeColor;
+    }
+    
     private void Update()
     {
+        currentParticleSystem.transform.position = pouringPoint.transform.position;
         if (isPressed)
         {
+            ParticleSystem.EmissionModule emission = currentParticleSystem.emission;
+            emission.rateOverTime = 20f;
             if (Physics.Raycast(pouringPoint.transform.position, Vector3.down, out RaycastHit hit, float.MaxValue, PlayerData.GetInstance().cupLayer))
             {
                 Cup cup = hit.collider.gameObject.GetComponent<Cup>();
                 if (cup != null)
                     cup.IncreaseCoffee(basePouringSpeed * Time.deltaTime);
             }
+        }
+        else
+        {
+            ParticleSystem.EmissionModule emission = currentParticleSystem.emission;
+            emission.rateOverTime = 0f;
         }
     }
 
